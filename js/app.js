e1f50799cccbf4718dd01fd9c96ee695777d6fd8
Bonacina Alberto <bonacina.alberto@gmail.com>
2020-03-28 07:42:52 +0100

    feat: Aggiunta chiamata per scaricare tutti i dati dal repository della protezione civile

--- js/app.js
+++ js/app.js
@@ -169,6 +169,22 @@ $(function (e) {
         });
         $("#select_filtro").html(opzioniSelect);
     }
+
+    //Vado a scaricare i due JSON per andamento nazionale e regioni dal repository della protezione civile
+    $.getJSON('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json', function (data) {
+        
+        arrayDataNazionale = data;
+
+        $.getJSON('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json', function (data) {
+
+            arrayDataRegioni = data;
+            
+            //Inizializzo la pagina con i valori nazionali
+            compilaPaginaConValoriGrafici(arrayDataNazionale);
+
+            //Creo la select per filtrare i dati
+            creaSelectRegioni();
+        })
     })
 
 })
